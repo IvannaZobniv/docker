07d4b3bc57e5d76182e4323fde648549ecdf0999
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-03-06 22:41:09 +0200

    lesson3

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import fs from "fs"
+import express, { Request, Response } from "express";
 
 const app = express();
 //
 // app.use(express.json());
 
 const PORT = 5100;
-app.listen(PORT, ()=>{
-    console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
+app.listen(PORT, () => {
+  console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
 });
-
 
 // app.post()
 // app.put()
@@ -22,78 +22,78 @@ app.listen(PORT, ()=>{
 // });
 
 const users = [
-    {
-        name: 'Oleh',
-        age: 19,
-        gender: 'male'
-    },
-    {
-        name: 'Anton',
-        age: 22,
-        gender: 'female'
-    },
-    {
-        name: 'Anya',
-        age: 25,
-        gender: 'female'
-    },
-    {
-        name: 'Ielizavetta',
-        age: 35,
-        gender: 'female'
-    },
-    {
-        name: 'Cocos',
-        age: 70,
-        gender: 'mixed'
-    }
-]
+  {
+    name: "Oleh",
+    age: 19,
+    gender: "male",
+  },
+  {
+    name: "Anton",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "Anya",
+    age: 25,
+    gender: "female",
+  },
+  {
+    name: "Ilizavetta",
+    age: 35,
+    gender: "female",
+  },
+  {
+    name: "Cocos",
+    age: 70,
+    gender: "mixed",
+  },
+];
+
 // якщо хочемо отримати всіх юзера
-app.get('/users', (req, res)=>{
-    res.json(users);
+app.get("/users", (req: Request, res: Response) => {
+  res.json(users);
 });
 
 // якщо хочемо отримати одного юзера (фільтрація)
-app.get('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
-    const user = users[+userId-1];
+app.get("/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = users[+userId - 1];
 
-    res.json(user);
+  res.json(user);
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // створити юзера
-app.post('/users', (req, res)=>{
-    const body = req.body;
-    users.push(body);
-    console.log(body);
+app.post("/users", (req, res) => {
+  const body = req.body;
+  users.push(body);
+  console.log(body);
 
-    res.status(201).json({
-        message: 'User created!'
-    })
-})
+  res.status(201).json({
+    message: "User created!",
+  });
+});
 //якщо ми хочемо оновити юзера
-app.put('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
-    const updatedUser = req.body;
+app.put("/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  const updatedUser = req.body;
 
-    users[+userId] = updatedUser;
+  users[+userId] = updatedUser;
 
-    res.status(200).json({
-        message: 'User updated',
-        data: users[+userId]
-    })
-})
+  res.status(200).json({
+    message: "User updated",
+    data: users[+userId],
+  });
+});
 //якщо ми хочемо видалити юзера
-app.delete('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
+app.delete("/users/:userId", (req, res) => {
+  const { userId } = req.params;
 
-    users.splice(+userId, 1);
+  users.splice(+userId, 1);
 
-    res.status(200).json({
-        message: 'User deleted',
-    })
-})
-
+  res.status(200).json({
+    message: "User deleted",
+  });
+});
